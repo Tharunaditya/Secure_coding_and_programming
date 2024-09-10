e15751ddb79fa93df3265625d893608a6b1cb5f5
Tharunaditya Anuganti <anuganti9@gmail.com>
2024-09-10 09:59:56 +0530

    new commit

--- labs/week3/02_01_if_greater_than.py
+++ labs/week3/02_01_if_greater_than.py
@@ -4,10 +4,11 @@
 # you can use the function `input()` to get user input
 
 # example
-first_number = input("please enter a number: ")
-second_number = input("please enter another number!: ")
+first_number = int(input("please enter a number: "))
+second_number = int(input("please enter another number!: "))
+result = "first number is greater" if first_number>second_number else "second number is greater"
 
-print(first_number)
+print(result)
 
 ## note, what type is your first_number? Is it what you expected?
 ## look-up the input() documentation to find out what type it will create by default
--- labs/week3/02_02_if_else_game.py
+++ labs/week3/02_02_if_else_game.py
@@ -1,16 +1,28 @@
 # write a short command line game
-
+name = str(input("Please enter your name:"))
 # 1. ask the user for their name: (use the input() function)
+print(f"Hello {name}")
 
 # Say hello to them with their name
 # If their name begins with a vowel say something funny about that ("Aha! Oho! Uhu! Ihi! etc", your name begins with a vowel!)
 # If their n ame begins with a consonant make an even better joke about it.
+vowels = ['a','e','i','o','u']
+if name[0].lower() in vowels:
+    print(f" Aha! Oho! Uhu! Ihi! etc your {name} starts with vowel")
+else:
+    print(f"yellow yellow {name} is dirty fellow because your name starts with consonant")
 
 # Ask them to pick a number between 1 and 10.
 # If they guessed the right number, tell them they won
 # Else, tell them they lost.
-
 import random
 
 random_number = random.randint(0, 10)
+
+numb=int(input("Please enter a number between 1 and 10: "))
+
+if random_number == numb:
+    print("Hurray ! your number is correct")
+else:
+    print("You are wrong !!")
 
